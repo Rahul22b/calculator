911ccc8d61ff71a22c1f3c77b4a3db2f4bbd68f5
Rahul <168935427+Rahul22b@users.noreply.github.com>
2024-11-17 19:15:39 +0530

    Update Button.jsx
    add cross btn function

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -19,6 +19,8 @@ export default function Button({e,setval,val}) {
         const b=eval(val);
         setval(`Ans ${b}`);
       }
+       else if(e==='X'){
+        setval(val.slice(0,-1));
       else{
         setval(val+e)
       }
