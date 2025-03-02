8e54ee4245878f8d7df583a6e6d08f75d8c13fc4
rahul22b <rahul2212005@gmail.com>
2025-03-02 17:33:01 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import Display from "./Display.jsx";
-import Button from "./component/Button.jsx";
 import Heading from "./component/Heading.jsx";
 import Keypad from "./Keypad.jsx";
 import "./App.css";
@@ -11,12 +10,14 @@ export default function App() {
   return (
     <div className="calculator">
       <Heading></Heading>
-      {/* <input type="text" className="display" value={val}/> */}
+      
       <Display value={val}></Display>
       
       <div className="keypad">
         <Keypad setval={setval} val={val}></Keypad>
       </div>
+
+      <div className="footer">Made by ❤️ for Sana</div>
     </div>
   );
 }
